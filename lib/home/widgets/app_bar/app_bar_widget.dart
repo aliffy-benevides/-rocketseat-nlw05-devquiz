d61be67478d0846f444ba7ff10d3ac68f6f836c7
Aliffy Benevides <aliffy.benevides@outlook.com>
2021-04-19 15:38:18 -0300

    First class

---
+++ lib/home/widgets/app_bar/app_bar_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:quizdev/core/core.dart';
+import 'package:quizdev/home/widgets/score_card/score_card_widget.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 161,
+                    width: double.maxFinite,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    decoration: BoxDecoration(gradient: AppGradients.linear),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(TextSpan(children: [
+                          TextSpan(text: "Olá, ", style: AppTextStyles.title),
+                          TextSpan(
+                              text: "Aliffy DEV",
+                              style: AppTextStyles.titleBold),
+                        ])),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/56937947?v=4"),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(0.0, 1.0),
+                    child: ScoreCardWidget(),
+                  )
+                ],
+              ),
+            ));
+}
